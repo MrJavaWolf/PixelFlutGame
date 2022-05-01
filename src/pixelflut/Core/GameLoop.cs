a65b0dadfac7612274fae9420fdddbf8249057fb
Jens Larsen <mr.javawolf@gmail.com>
2022-05-01 19:03:19 +0200

    Added: Gameloop stats

--- src/pixelflut/Core/GameLoop.cs
+++ src/pixelflut/Core/GameLoop.cs
@@ -11,16 +11,28 @@ namespace PixelFlut.Core
         public int NumberOfRenderer { get; set; }
     }
 
+    public class GameLoopStats
+    {
+        public GameTime Time { get; set; } = new();
+        public int SleepTime { get; set; }
+        public int Frames { get; set; }
+    }
+
     public class GameLoop
     {
+        private readonly ILogger<GameLoop> logger;
         private readonly IServiceProvider provider;
         private readonly GameLoopConfiguration configuration;
+        private GameLoopStats stats = new();
+        private Stopwatch stopwatch = new();
 
         public GameLoop(ILogger<GameLoop> logger, IServiceProvider provider, GameLoopConfiguration configuration)
         {
+            this.logger = logger;
             this.provider = provider;
             this.configuration = configuration;
             logger.LogInformation($"GameLoop: {{@configuration}}", configuration);
+            stopwatch.Start();
         }
 
         public void Run(CancellationToken cancellationToken)
@@ -50,7 +62,18 @@ namespace PixelFlut.Core
                 foreach (var renderer in renderers)
                     renderer.PrepareRender(numberOfPixels, frame);
                 int sleepTimeMs = Math.Max(1, (int)(1000.0 / configuration.TargetGameLoopFPS - loopTime.Elapsed.TotalMilliseconds));
+                stats.Frames++;
+                if (stopwatch.ElapsedMilliseconds > 1000)
+                {
+                    stats.SleepTime = sleepTimeMs;
+                    stats.Time = gameTime;
+                    logger.LogInformation("Gameloop: {@stats}", stats);
+                    stats = new GameLoopStats();
+                    stopwatch.Restart();
+                }
+
                 Thread.Sleep(sleepTimeMs);
+
             }
         }
 

11cd494e17c1b4c8f490596d5d4fc310536c766d
Jens Larsen <mr.javawolf@gmail.com>
2022-05-15 18:01:21 +0200

    Improved: Cleanup

--- src/pixelflut/Core/MathHelper.cs
+++ src/pixelflut/Core/MathHelper.cs
@@ -1,45 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Drawing;
-using System.Linq;
+﻿using System.Drawing;
 using System.Numerics;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace PixelFlut.Core
+namespace PixelFlut.Core;
+
+public static class MathHelper
 {
-    public static class MathHelper
+    public static float RemapRange(float from, float fromMin, float fromMax, float toMin, float toMax)
     {
-        public static float RemapRange(float from, float fromMin, float fromMax, float toMin, float toMax)
-        {
-            var fromAbs = from - fromMin;
-            var fromMaxAbs = fromMax - fromMin;
+        var fromAbs = from - fromMin;
+        var fromMaxAbs = fromMax - fromMin;
 
-            var normal = fromAbs / fromMaxAbs;
+        var normal = fromAbs / fromMaxAbs;
 
-            var toMaxAbs = toMax - toMin;
-            var toAbs = toMaxAbs * normal;
+        var toMaxAbs = toMax - toMin;
+        var toAbs = toMaxAbs * normal;
 
-            var to = toAbs + toMin;
+        var to = toAbs + toMin;
 
-            return to;
-        }
+        return to;
+    }
 
-        public static Vector2 Rotate(Vector2 v, double degrees)
-        {
-            return new Vector2(
-                (float)(v.X * Math.Cos(degrees) - v.Y * Math.Sin(degrees)),
-                (float)(v.X * Math.Sin(degrees) + v.Y * Math.Cos(degrees))
-            );
-        }
+    public static Vector2 Rotate(Vector2 v, double degrees)
+    {
+        return new Vector2(
+            (float)(v.X * Math.Cos(degrees) - v.Y * Math.Sin(degrees)),
+            (float)(v.X * Math.Sin(degrees) + v.Y * Math.Cos(degrees))
+        );
+    }
 
-        public static Color Lerp(this Color startColor, Color endColor, float amount)
-        {
-            Vector4 vectorStart = new(startColor.R, startColor.G, startColor.B, startColor.A);
-            Vector4 vectorEnd = new(endColor.R, endColor.G, endColor.B, endColor.A);
-            Vector4 resultVector = Vector4.Lerp(vectorStart, vectorEnd, amount);
-            Color resultColor = Color.FromArgb((int)resultVector.W, (int)resultVector.X, (int)resultVector.Y, (int)resultVector.Z);
-            return resultColor;
-        }
+    public static Color Lerp(this Color startColor, Color endColor, float amount)
+    {
+        Vector4 vectorStart = new(startColor.R, startColor.G, startColor.B, startColor.A);
+        Vector4 vectorEnd = new(endColor.R, endColor.G, endColor.B, endColor.A);
+        Vector4 resultVector = Vector4.Lerp(vectorStart, vectorEnd, amount);
+        Color resultColor = Color.FromArgb((int)resultVector.W, (int)resultVector.X, (int)resultVector.Y, (int)resultVector.Z);
+        return resultColor;
     }
 }
--- src/pixelflut/Core/PixelFlutScreenSender.cs
+++ src/pixelflut/Core/PixelFlutScreenSender.cs
@@ -1,87 +1,86 @@
 ﻿using System.Net;
 using System.Net.Sockets;
 
-namespace PixelFlut.Core
+namespace PixelFlut.Core;
+
+public class PixelFlutScreenSender
 {
-    public class PixelFlutScreenSender
+    private readonly PixelFlutScreenConfiguration configuration;
+
+    // Connection
+    private Socket socket;
+    private IPEndPoint endPoint;
+
+    private int currentRenderFrameBuffer = 0;
+    private int currentRenderByteBuffer = 0;
+
+
+    public PixelFlutScreenSender(PixelFlutScreenConfiguration configuration)
     {
-        private readonly PixelFlutScreenConfiguration configuration;
+        this.configuration = configuration;
 
-        // Connection
-        private Socket socket;
-        private IPEndPoint endPoint;
+        // Setup connnection
+        socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+        IPAddress serverAddr = IPAddress.Parse(configuration.Ip);
+        endPoint = new IPEndPoint(serverAddr, configuration.Port);
+    }
 
-        private int currentRenderFrameBuffer = 0;
-        private int currentRenderByteBuffer = 0;
+    public void Render(List<PixelBuffer> frame, PixelFlutScreenStats stats)
+    {
+        if (frame.Count == 0) return;
 
+        // Pick a buffer to render
+        (int pixels, byte[] sendBuffer) = SelectNextBuffer(frame);
 
-        public PixelFlutScreenSender(PixelFlutScreenConfiguration configuration)
+        // Send 
+        int bytesSent = socket.SendTo(sendBuffer, endPoint);
+
+        // Update stats
+        stats.BytesSent += bytesSent;
+        stats.PixelsSent += pixels;
+        stats.BuffersSent++;
+        stats.TotalBuffersSent++;
+
+        // Wait if requested, usefull on slower single core CPU's
+        if (configuration.SleepTimeBetweenSends != -1)
         {
-            this.configuration = configuration;
-         
-            // Setup connnection
-            socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
-            IPAddress serverAddr = IPAddress.Parse(configuration.Ip);
-            endPoint = new IPEndPoint(serverAddr, configuration.Port);
+            Thread.Sleep(configuration.SleepTimeBetweenSends);
+        }
+    }
+
+    private (int pixels, byte[] sendBuffer) SelectNextBuffer(List<PixelBuffer> frame)
+    {
+        // Ensures we reset if the frame changes
+        if (frame.Count <= currentRenderFrameBuffer)
+        {
+            currentRenderFrameBuffer = 0;
+            currentRenderByteBuffer = 0;
         }
 
-        public void Render(List<PixelBuffer> frame, PixelFlutScreenStats stats)
+        // Gets the buffer
+        PixelBuffer buffer = frame[currentRenderFrameBuffer];
+
+        // Ensures we reset if the frame changes
+        if (buffer.Buffers.Count <= currentRenderByteBuffer)
         {
-            if (frame.Count == 0) return;
-
-            // Pick a buffer to render
-            (int pixels, byte[] sendBuffer) = SelectNextBuffer(frame);
-
-            // Send 
-            int bytesSent = socket.SendTo(sendBuffer, endPoint);
-
-            // Update stats
-            stats.BytesSent += bytesSent;
-            stats.PixelsSent += pixels;
-            stats.BuffersSent++;
-            stats.TotalBuffersSent++;
-
-            // Wait if requested, usefull on slower single core CPU's
-            if (configuration.SleepTimeBetweenSends != -1)
-            {
-                Thread.Sleep(configuration.SleepTimeBetweenSends);
-            }
+            currentRenderByteBuffer = 0;
         }
 
-        private (int pixels, byte[] sendBuffer) SelectNextBuffer(List<PixelBuffer> frame)
+        // Sends the buffer
+        byte[] sendBuffer = buffer.Buffers[currentRenderByteBuffer];
+        int pixelsPerBuffer = buffer.PixelsPerBuffer;
+
+        // Increment to select the next buffer
+        currentRenderByteBuffer++;
+        if (currentRenderByteBuffer >= buffer.Buffers.Count)
         {
-            // Ensures we reset if the frame changes
-            if(frame.Count <= currentRenderFrameBuffer)
+            currentRenderByteBuffer = 0;
+            currentRenderFrameBuffer++;
+            if (currentRenderFrameBuffer >= frame.Count)
             {
                 currentRenderFrameBuffer = 0;
-                currentRenderByteBuffer = 0;
             }
-            
-            // Gets the buffer
-            PixelBuffer buffer = frame[currentRenderFrameBuffer];
-
-            // Ensures we reset if the frame changes
-            if (buffer.Buffers.Count <= currentRenderByteBuffer)
-            {
-                currentRenderByteBuffer = 0;
-            }
-            
-            // Sends the buffer
-            byte[] sendBuffer = buffer.Buffers[currentRenderByteBuffer];
-            int pixelsPerBuffer = buffer.PixelsPerBuffer;
-
-            // Increment to select the next buffer
-            currentRenderByteBuffer++;
-            if (currentRenderByteBuffer >= buffer.Buffers.Count)
-            {
-                currentRenderByteBuffer = 0;
-                currentRenderFrameBuffer++;
-                if (currentRenderFrameBuffer >= frame.Count)
-                {
-                    currentRenderFrameBuffer = 0;
-                }
-            }
-            return (pixelsPerBuffer, sendBuffer);
         }
+        return (pixelsPerBuffer, sendBuffer);
     }
 }
